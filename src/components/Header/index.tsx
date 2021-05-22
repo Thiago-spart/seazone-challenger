bd977a30175ab676163f58962866bd035e0d0069
Thiago Spart <user000.000resu@gmail.com>
2021-05-22 14:37:31 -0300

    Filter Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,37 +1,43 @@
 import { FaUserAlt } from "react-icons/fa";
 import { TiArrowSortedDown } from "react-icons/ti";
+import { Filter } from "./components/Filter";
 
 import styles from "./styles.module.scss";
 
 export function Header() {
   return (
-    <header className={styles.headerContainer}>
-      <img src="/logo-header.png" alt="seazone logo" />
+    <header>
+      <div className={styles.headerContainer}>
+        <img src="/logo-header.png" alt="seazone logo" />
 
-      <a href="#" type="button">
-        Calendário
-      </a>
-      <a href="#" type="button">
-        Clientes
-      </a>
-      <a href="#" type="button">
-        Orçamento
-      </a>
-      <a href="#" type="button">
-        Propriedades
-      </a>
-      <a href="#" type="button">
-        Controle
-      </a>
+        <a href="#" type="button">
+          Calendário
+        </a>
+        <a href="#" type="button">
+          Clientes
+        </a>
+        <a href="#" type="button">
+          Orçamento
+        </a>
+        <a href="#" type="button">
+          Propriedades
+        </a>
+        <a href="#" type="button">
+          Controle
+        </a>
 
-      <div>
         <div>
-          <FaUserAlt />
-        </div>
-        <div>
-          <TiArrowSortedDown />
+          <div>
+            <FaUserAlt />
+          </div>
+          <div>
+            <TiArrowSortedDown />
+          </div>
         </div>
       </div>
+      <>
+        <Filter />
+      </>
     </header>
   );
 }
