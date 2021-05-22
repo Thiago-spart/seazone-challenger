3ff74e89dc1124513296a6d09736b9edc0e41d86
Thiago Spart <user000.000resu@gmail.com>
2021-05-22 16:08:48 -0300

    Remove wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,10 @@ import "../styles/global.scss";
 import { Header } from "../components/Header";
 import { PlayerContextProvider } from "../context/PlayerContext";
 
-import styles from "../styles/app.module.scss";
-
 function MyApp({ Component, pageProps }) {
   return (
     <PlayerContextProvider>
-      <div className={styles.wrapper}>
+      <div>
         <main>
           <Header />
           <Component {...pageProps} />
