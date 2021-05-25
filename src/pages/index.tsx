19d1155893c2c029461f09150e251299edf5df56
Thiago Spart <user000.000resu@gmail.com>
2021-05-25 16:11:15 -0300

    Properties section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,173 +1,146 @@
-import { GetStaticProps } from "next"
-import Image from 'next/image'
-import Head from 'next/head'
-import Link from 'next/link'
-import { format, parseISO } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import Link from "next/link";
 
-import { api } from '../services/api'
-import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
-import { usePlayer } from "../context/PlayerContext"
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+import { TiArrowSortedDown, TiHome, TiMap } from "react-icons/ti";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
-import styles from '../pages/home.module.scss'
+import styles from "../pages/home.module.scss";
 
-type Episode = {
-  id: string
-  title: string
-  thumbnail: string
-  members: string
-  duration: number
-  durationAsString: string
-  url: string
-  publishedAt: string
-}
+type Property = {
+  id: number;
+  name: string;
+  room: number;
+  street: string;
+  maxHosts: number;
+  cleaningTax: number;
+  guarantee: number;
+  daily: number;
+  img: string;
+};
 
 type HomeProps = {
-  latestEpisodes: Episode[]
-  allEpisodes: Episode[]
-}
+  properties: Property[];
+};
 
-export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-  const { playList } = usePlayer()
-
-  const episodeList = [...latestEpisodes, ...allEpisodes]
-
+export default function Home({ properties }: HomeProps) {
+  const propertyList = [...properties];
   return (
-
     <div className={styles.homepage}>
-      
       <Head>
         <title>Home | Seazone </title>
       </Head>
-      
-      <section className={styles.latestEpisodes}>
-        <h2>Últimos lançamentos</h2>
 
-        <ul>
-          {latestEpisodes.map((episode, index) => {
-            return (
-              <li key={episode.id}>
-                <Image 
-                  width={192} 
-                  height={192} 
-                  src={episode.thumbnail} 
-                  alt={episode.title}
-                  objectFit="cover" 
-                 />
-
-                <div className={styles.episodeDetails}>
-                  <Link href={`/episodes/${episode.id}`}>
-                    <a>{episode.title}</a>
-                  </Link>
-                  <p>{episode.members}</p>
-                  <span>{episode.publishedAt}</span>
-                  <span>{episode.durationAsString}</span>
-                </div>
-
-                <button 
-                  type="button" 
-                  onClick={() => playList(episodeList, index)}
-                >
-                  <img src="/play-green.svg" alt="Tocar episódio" />
-                </button>
-              </li>
-             )
-            })}
-          </ul>
+      <section className={styles.sectionContainer}>
+        <div className={styles.titleSection}>
+          <h2>Todos os imóveis</h2>
+          <span>{propertyList.length} ímoveis</span>
+        </div>
+        <div className={styles.filterButton}>
+          <button>
+            Ordenar
+            <TiArrowSortedDown />
+          </button>
+        </div>
       </section>
-      
-      <section className={styles.allEpisodes}>
-        <h2>Todos episódios</h2>
 
-        <table cellSpacing={0}>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Podcast</th>
-              <th className={styles.integrantes}>Integrantes</th>
-              <th>Data</th>
-              <th className={styles.duracao}>Duração</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {allEpisodes.map((episode, index) => {
-              return (
-                <tr key={episode.id}>
-                  <td className={styles.toFitSize}>
-                    <Image 
-                      width={120}
-                      height={120}
-                      src={episode.thumbnail}
-                      alt={episode.title}
-                      objectFit="cover"
-                    />
-                  </td>
-                  <td>
-                    <Link href={`/episodes/${episode.id}`}>
-                      <a>{episode.title}</a>
-                    </Link>
-                  </td>
-                  <td className={styles.integrantes}>{episode.members}</td>
-                  <td className={styles.toFitSize} style={{ width: 100 }}>{episode.publishedAt}</td>
-                  <td className={styles.duracao}>{episode.durationAsString}</td>
-                  <td>
-                    <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)}>
-                      <img src="/play-green.svg" alt="Tocar episódio"/>
-                    </button>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+      <section className={styles.cardContainer}>
+        {propertyList.map(property => {
+          return (
+            <div key={property.id} className={styles.propertyCard}>
+              <div>
+                <div className={styles.imgContainer}>
+                  <Link href={`/property/${property.id}`}>
+                    <img src={property.img} alt={property.name} />
+                  </Link>
+                  <div className={styles.iconCard}>
+                    <div>
+                      <TiMap />
+                    </div>
+                    <div>
+                      <TiHome />
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.cardHeader}>
+                  <div>
+                    <h2>{property.name}</h2>
+                    <p>
+                      {property.street} . {property.maxHosts} hóspedes .{" "}
+                      {property.room} quartos
+                    </p>
+                  </div>
+                  <span>
+                    <BsThreeDotsVertical />
+                  </span>
+                </div>
+                <div className={styles.cardBody}>
+                  <div className={styles.price}>
+                    <span>Valor diária</span>
+                    <span>R${property.daily} / noite</span>
+                  </div>
+                  <div className={styles.price}>
+                    <span>Total 5 diárias</span>
+                    <span>R${property.daily * 5}</span>
+                  </div>
+                  <div className={styles.price}>
+                    <span>Taxa de limpeza</span>
+                    <span>R${property.cleaningTax}</span>
+                  </div>
+                  <div className={styles.price}>
+                    <span>Caução</span>
+                    <span>R${property.guarantee}</span>
+                  </div>
+                  <div className={styles.totalPrice}>
+                    <span>Total:</span>
+                    <div>
+                      <span>
+                        R${5 * property.daily + property.cleaningTax} |
+                      </span>
+                      <div>
+                        <span>
+                          R${5 * property.daily + property.cleaningTax}
+                        </span>
+                        <p>à vista</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </section>
     </div>
-
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get('episodes', {
+  const { data } = await api.get("properties", {
     params: {
-      _limit: 12,
-      _sort: 'published_at',
-      _order: 'desc'
-    }
-  })
-
-  const episodes = data.map(episode => {
+      _limit: 6,
+    },
+  });
+  const properties = data.map(property => {
     return {
-      id: episode.id,
-      title: episode.title,
-      thumbnail: episode.thumbnail,
-      menbers: episode.members,
-      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
-        locale: ptBR 
-      }),
-      duration: Number(episode.file.duration),
-      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      url: episode.file.url,
-    }
-  })
-
-  const latestEpisodes = episodes.slice(0, 2)
-  const allEpisodes = episodes.slice(2, episodes.length)
+      id: property.id,
+      name: property.name,
+      room: property.description.room,
+      street: property.address.street,
+      maxHosts: property.maxHosts,
+      cleaningTax: property.cleaningTax,
+      guarantee: property.guarantee,
+      daily: property.daily,
+      img: property.img,
+    };
+  });
 
   return {
-  	props: {
-  		latestEpisodes,
-      allEpisodes,
-  	},
-  	revalidate: 60 * 60 * 8, //8 HRs
-  }
-}
-
-//SPA
-//SSR
-//SSG
-//Responsivo
-//PWA
-//Tema Dark
-//Omini theme
-//eletricon
+    props: {
+      properties,
+    },
+    revalidate: 60 * 60 * 8, //8 HRs
+  };
+};
