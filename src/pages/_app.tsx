dcebda72b866e723cfea464b37209e1c5f27eab0
Thiago Spart <user000.000resu@gmail.com>
2021-05-27 01:48:02 -0300

    Delete playerContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,15 @@
 import "../styles/global.scss";
 
 import { Header } from "../components/Header";
-import { PlayerContextProvider } from "../context/PlayerContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlayerContextProvider>
-      <div>
-        <main>
-          <Header />
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </PlayerContextProvider>
+    <div>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    </div>
   );
 }
 
