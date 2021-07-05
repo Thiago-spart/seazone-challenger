d15e741f580ac2c052e21f22176e8b4c45ff6377
Thiago Moraes <user000.000resu@gmail.com>
2021-07-05 00:36:12 -0300

    Refatoring Filter component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -4,57 +4,49 @@ import styles from "./styles.module.scss";
 
 export function Filter() {
   return (
-    <form>
-      <div className={styles.filterContainer}>
-        <div className={styles.formGroup}>
-          <label htmlFor="localization">Localização</label>
-          <select
-            id="localization"
-            name="localization"
-            className={styles.formLocation}
-          >
-            <option value="florianopolis">Florianópolis</option>
-            <option value="sao-paulo">São Paulo</option>
-            <option value="rio-de-janeiro">Rio de Janeiro</option>
-            <option value="recife">Recife</option>
-            <option value="Caruaru">caruaru</option>
-            <option value="petrolina">Petrolina</option>
-          </select>
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="price">Preço</label>
-          <select id="price" name="price" className={styles.formPrice}>
-            <option value="300-10000">300-10000</option>
-            <option value="10000-15000">10000-15000</option>
-            <option value="15000-20000">15000-20000</option>
-            <option value="20000-25000">20000-25000</option>
-            <option value="25000">25000+</option>
-          </select>
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="check-in">Check-in</label>
-          <input id="check-in" type="date" className={styles.formInput} />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="check-out">Check-out</label>
-          <input id="check-out" type="date" className={styles.formInput} />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="numeroHospedes">N° de hóspedes</label>
-          <input
-            type="number"
-            min="2"
-            id="numeroHospedes"
-            name="numeroHospedes"
-            className={styles.formInput}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <button className={styles.searchButton}>Buscar</button>
-        </div>
-        <div className={styles.formGroup}>
-          <div className={styles.moreFilters}>+ Mais filtros</div>
-        </div>
+    <form className={styles.container}>
+      <div className={styles.formGroup}>
+        <label htmlFor="localization">Localização</label>
+        <select
+          id="localization"
+          name="localization"
+          className={styles.formLocation}
+        >
+          <option value="florianopolis">Florianópolis</option>
+          <option value="sao-paulo">São Paulo</option>
+          <option value="rio-de-janeiro">Rio de Janeiro</option>
+          <option value="recife">Recife</option>
+          <option value="Caruaru">caruaru</option>
+          <option value="petrolina">Petrolina</option>
+        </select>
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="price">Preço</label>
+        <select id="price" name="price" className={styles.formPrice}>
+          <option value="300-10000">300-10000</option>
+          <option value="10000-15000">10000-15000</option>
+          <option value="15000-20000">15000-20000</option>
+          <option value="20000-25000">20000-25000</option>
+          <option value="25000">25000+</option>
+        </select>
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="check-in">Check-in</label>
+        <input id="check-in" type="date" />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="check-out">Check-out</label>
+        <input id="check-out" type="date" />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="GhestsNumber">N° de hóspedes</label>
+        <input type="number" min="2" id="GhestsNumber" name="GhestsNumber" />
+      </div>
+      <div className={styles.formGroup}>
+        <button type="submit">Buscar</button>
+      </div>
+      <div className={styles.formGroup}>
+        <div className={styles.moreFilters}>+ Mais filtros</div>
       </div>
     </form>
   );
